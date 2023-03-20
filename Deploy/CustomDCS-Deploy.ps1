16c0fd12656b44c17c63445eb1ed00fe123567ce
Brendan Law <brendan.law.contractor@pepsico.com>
2023-03-20 09:39:25 -0700

    changed to get path from registry

--- Deploy/CustomDCS-Deploy.ps1
+++ Deploy/CustomDCS-Deploy.ps1
@@ -1,6 +1,7 @@
 ## Script to backup the current Mi8 auto start script and replace with our custom one from the current repo
-$ErrorActionPreference = "Stop"
-$macroSequenciesRelPath = "Mods\aircraft\Mi-8MTV2\Cockpit\Scripts\Macro_sequencies.lua"
+$ErrorActionPreference = "Stop" # this is a debug setting, will stop on any error so that we can figure out what went wrong
+$macroSequenciesRelPath = "Mods\aircraft\{0}\Cockpit\Scripts\Macro_sequencies.lua"
+$Mi8MTV2 = "Mi-8MTV2"
 
 Write-Host "`n** Custom DCS deployment script **`n"
 
@@ -21,11 +22,13 @@ function Get-Folder()
 
 function Get-DCSInstallPath {
   # Check that we know where the ED install location is:
-  $installPath = $env:DCS_INSTALL_PATH
+  ##$installPath = $env:DCS_INSTALL_PATH # the environment variables don't seem to persist, so let's use the registry instead
+  $installPath = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\DCS World OpenBeta_is1' -Name InstallLocation).InstallLocation
   if(!$installPath)
   {
     Write-Host "Where is your DCS install path?"
-    $env:DCS_INSTALL_PATH = Get-Folder
+    $installPath = Get-Folder
+    Write-Host "If this keeps happening, please reach out to FlamerNZ..."
   }
   return $installPath
 }
@@ -43,28 +46,30 @@ function Get-BackupPath ($path, $i = 0) {
   return $path
 }
 
+
 $installPath = Get-DCSInstallPath
 Write-Host "Current DCS install path: " $installPath
 
-$macroSequenciesPath = $installPath + "\" + $macroSequenciesRelPath
-Write-Host "Checking that I can find your autostart file..."
+$macroSequenciesPath = $installPath + "\" + ([string]::Format($macroSequenciesRelPath,$Mi8MTV2))
+Write-Host "Checking that we can find your autostart file..."
 if(!(Test-Path $macroSequenciesPath))
 {
   Write-Error -Message "File doesn't seem to be at this path: $macroSequenciesPath"
-  $response = Read-Host -Prompt "Would you like to reset your DCS install path? (Y/N)"
+  $response = Read-Host -Prompt "Would you like to select your DCS install path manually? (Y/N)"
   if($response -eq "Y")
   {
     $installPath = Get-DCSInstallPath
   } else {
+    Write-Host "Installer Exiting"
     exit 1
   }
 }
 Write-Host "Taking a backup of your current auto start..."
 $backupPath = $macroSequenciesPath + "." + (get-date -Format "yy-MM-dd") + ".bak"
 $backupPath = Get-BackupPath $backupPath
-Write-Host $backupPath
-Write-Host $macroSequenciesPath
+#Write-Host $macroSequenciesPath
 Rename-Item $macroSequenciesPath -NewName $backupPath
+Write-Host "Backup saved to: " $backupPath
 
 Write-Host "Deploying new auto start..."
 Copy-Item "Startup\Mi-8\Macro_sequencies.lua" -Destination $macroSequenciesPath
