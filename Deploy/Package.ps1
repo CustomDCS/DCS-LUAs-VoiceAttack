628c227391d043eeb5fd2cc2c423ebf5529472c9
Brendan Law <brendan.law.contractor@pepsico.com>
2023-04-06 07:42:22 -0700

    Packaging script added to create zip and exes

---
+++ Deploy/Package.ps1
@@ -0,0 +1,6 @@
+# zip all the files in Mods\
+Compress-Archive -Path .\Mods "CustomDCS.zip"
+# exe latest deploy script
+Invoke-PS2EXE .\Deploy\CustomDCS-Deploy.ps1 -outputFile "CustomDCS.exe"
+# copy to output folder
+Move-Item "CustomDCS.*" .\Output\.
