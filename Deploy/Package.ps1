cad13c865e9a7de523efbc634b8b8d50c41f92d0
Brendan Law <brendan.law.contractor@pepsico.com>
2023-04-15 12:59:24 -0700

    check if folder exists

--- Deploy/Package.ps1
+++ Deploy/Package.ps1
@@ -4,5 +4,9 @@ Remove-Item .\Output\* -Force -Recurse
 Compress-Archive -Path .\Mods "CustomDCS.zip"
 # exe latest deploy script
 Invoke-PS2EXE .\Deploy\CustomDCS-Deploy.ps1 -outputFile "CustomDCS.exe"
+
+if (!(Test-Path .\Output\)) {
+  mkdir Output
+}
 # copy to output folder
 Move-Item "CustomDCS.*" .\Output\.
