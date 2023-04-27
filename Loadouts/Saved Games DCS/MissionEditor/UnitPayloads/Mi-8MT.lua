1a1b6c032addea870782b639c12e058644c0b974
YushinMaru <phirejz@gmail.com>
2023-04-27 15:58:14 -0500

    Update Mi-8MT.lua

--- Loadouts/Saved Games DCS/MissionEditor/UnitPayloads/Mi-8MT.lua
+++ Loadouts/Saved Games DCS/MissionEditor/UnitPayloads/Mi-8MT.lua
@@ -2,7 +2,48 @@ local unitPayloads = {
 	["name"] = "Mi-8MT",
 	["payloads"] = {
 		[1] = {
-			["displayName"] = "AA Grenades",
+			["displayName"] = "HE/AT Rockets (80) | 2x23mm Cannon",
+			["name"] = "HE/AT Rockets (80) | 2x23mm Cannon",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "PKT_7_62",
+					["num"] = 8,
+				},
+				[2] = {
+					["CLSID"] = "KORD_12_7",
+					["num"] = 7,
+				},
+				[3] = {
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["num"] = 6,
+				},
+				[4] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 1,
+				},
+				[5] = {
+					["CLSID"] = "{05544F1A-C39C-466b-BC37-5BD1D52E57BB}",
+					["num"] = 2,
+				},
+				[6] = {
+					["CLSID"] = "{05544F1A-C39C-466b-BC37-5BD1D52E57BB}",
+					["num"] = 5,
+				},
+				[7] = {
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["num"] = 4,
+				},
+				[8] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 3,
+				},
+			},
+			["tasks"] = {
+				[1] = 35,
+			},
+		},
+		[2] = {
+			["displayName"] = "Grenades",
 			["name"] = "AA Grenades",
 			["pylons"] = {
 				[1] = {
@@ -34,48 +75,9 @@ local unitPayloads = {
 				[1] = 35,
 			},
 		},
-		[2] = {
-			["name"] = "AA DEATH MACHINE",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "PKT_7_62",
-					["num"] = 8,
-				},
-				[2] = {
-					["CLSID"] = "KORD_12_7",
-					["num"] = 7,
-				},
-				[3] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
-					["num"] = 6,
-				},
-				[4] = {
-					["CLSID"] = "GUV_VOG",
-					["num"] = 5,
-				},
-				[5] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
-					["num"] = 4,
-				},
-				[6] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
-					["num"] = 3,
-				},
-				[7] = {
-					["CLSID"] = "GUV_VOG",
-					["num"] = 2,
-				},
-				[8] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
-					["num"] = 1,
-				},
-			},
-			["tasks"] = {
-				[1] = 35,
-			},
-		},
 		[3] = {
-			["name"] = "AA Rockets",
+			["displayName"] = "AT Rockets (80) | Grenade Pod",
+			["name"] = "AT Rockets (80) | Grenade Pod",
 			["pylons"] = {
 				[1] = {
 					["CLSID"] = "PKT_7_62",
@@ -90,7 +92,7 @@ local unitPayloads = {
 					["num"] = 6,
 				},
 				[4] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["CLSID"] = "GUV_VOG",
 					["num"] = 5,
 				},
 				[5] = {
@@ -102,7 +104,7 @@ local unitPayloads = {
 					["num"] = 3,
 				},
 				[7] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["CLSID"] = "GUV_VOG",
 					["num"] = 2,
 				},
 				[8] = {
@@ -115,7 +117,8 @@ local unitPayloads = {
 			},
 		},
 		[4] = {
-			["name"] = "AA Standard Rockets",
+			["displayName"] = "AT Rockets (40) ",
+			["name"] = "AT Rockets (40) ",
 			["pylons"] = {
 				[1] = {
 					["CLSID"] = "PKT_7_62",
@@ -139,35 +142,40 @@ local unitPayloads = {
 			},
 		},
 		[5] = {
-			["name"] = "AA ROCKET MIX",
+			["displayName"] = "Door Gunners (NO PYLONS)",
+			["name"] = "Door Gunners (NO PYLONS)",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "KORD_12_7",
-					["num"] = 7,
-				},
-				[2] = {
-					["CLSID"] = "B_8V20A_OFP2",
+					["CLSID"] = "<CLEAN>",
 					["num"] = 6,
 				},
+				[2] = {
+					["CLSID"] = "<CLEAN>",
+					["num"] = 1,
+				},
 				[3] = {
-					["CLSID"] = "B_8V20A_OFP2",
-					["num"] = 5,
-				},
-				[4] = {
-					["CLSID"] = "B_8V20A_OFP2",
-					["num"] = 4,
-				},
-				[5] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
-					["num"] = 3,
-				},
-				[6] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["CLSID"] = "<CLEAN>",
 					["num"] = 2,
 				},
+				[4] = {
+					["CLSID"] = "<CLEAN-200.5>",
+					["num"] = 3,
+				},
+				[5] = {
+					["CLSID"] = "<CLEAN-200.5>",
+					["num"] = 4,
+				},
+				[6] = {
+					["CLSID"] = "<CLEAN>",
+					["num"] = 5,
+				},
 				[7] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
-					["num"] = 1,
+					["CLSID"] = "PKT_7_62",
+					["num"] = 8,
+				},
+				[8] = {
+					["CLSID"] = "KORD_12_7",
+					["num"] = 7,
 				},
 			},
 			["tasks"] = {
@@ -175,39 +183,32 @@ local unitPayloads = {
 			},
 		},
 		[6] = {
-			["name"] = "AA Standard",
+			["displayName"] = "Empty (No Pylons)",
+			["name"] = "Empty (No Pylons)",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "PKT_7_62",
-					["num"] = 8,
-				},
-				[2] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["CLSID"] = "<CLEAN>",
 					["num"] = 6,
 				},
-				[3] = {
-					["CLSID"] = "KORD_12_7",
-					["num"] = 7,
-				},
-				[4] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+				[2] = {
+					["CLSID"] = "<CLEAN>",
 					["num"] = 1,
 				},
-				[5] = {
-					["CLSID"] = "{05544F1A-C39C-466b-BC37-5BD1D52E57BB}",
-					["num"] = 5,
-				},
-				[6] = {
-					["CLSID"] = "{05544F1A-C39C-466b-BC37-5BD1D52E57BB}",
+				[3] = {
+					["CLSID"] = "<CLEAN>",
 					["num"] = 2,
 				},
-				[7] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+				[4] = {
+					["CLSID"] = "<CLEAN-200.5>",
+					["num"] = 3,
+				},
+				[5] = {
+					["CLSID"] = "<CLEAN-200.5>",
 					["num"] = 4,
 				},
-				[8] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
-					["num"] = 3,
+				[6] = {
+					["CLSID"] = "<CLEAN>",
+					["num"] = 5,
 				},
 			},
 			["tasks"] = {
@@ -215,39 +216,40 @@ local unitPayloads = {
 			},
 		},
 		[7] = {
-			["name"] = "AA AFIRST",
+			["displayName"] = "HE/AT Rockets (120)",
+			["name"] = "HE/AT Rockets (120)",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "PKT_7_62",
-					["num"] = 8,
-				},
-				[2] = {
 					["CLSID"] = "KORD_12_7",
 					["num"] = 7,
 				},
-				[3] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+				[2] = {
+					["CLSID"] = "B_8V20A_OFP2",
 					["num"] = 6,
+				},
+				[3] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 5,
 				},
 				[4] = {
 					["CLSID"] = "B_8V20A_OFP2",
-					["num"] = 1,
+					["num"] = 4,
 				},
 				[5] = {
-					["CLSID"] = "{05544F1A-C39C-466b-BC37-5BD1D52E57BB}",
-					["num"] = 2,
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["num"] = 3,
 				},
 				[6] = {
-					["CLSID"] = "{05544F1A-C39C-466b-BC37-5BD1D52E57BB}",
-					["num"] = 5,
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["num"] = 2,
 				},
 				[7] = {
 					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
-					["num"] = 4,
+					["num"] = 1,
 				},
 				[8] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
-					["num"] = 3,
+					["CLSID"] = "PKT_7_62",
+					["num"] = 8,
 				},
 			},
 			["tasks"] = {
@@ -255,32 +257,222 @@ local unitPayloads = {
 			},
 		},
 		[8] = {
-			["displayName"] = "AA  Clean DG",
-			["name"] = "AA  Clean DG",
+			["displayName"] = "AT Rockets (40) | 2x23mm Cannon",
+			["name"] = "AT Rockets (40) | 2x23mm Cannon",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "<CLEAN>",
-					["num"] = 6,
+					["CLSID"] = "PKT_7_62",
+					["num"] = 8,
 				},
 				[2] = {
-					["CLSID"] = "<CLEAN>",
-					["num"] = 1,
+					["CLSID"] = "KORD_12_7",
+					["num"] = 7,
 				},
 				[3] = {
-					["CLSID"] = "<CLEAN>",
-					["num"] = 2,
+					["CLSID"] = "{05544F1A-C39C-466b-BC37-5BD1D52E57BB}",
+					["num"] = 5,
 				},
 				[4] = {
-					["CLSID"] = "<CLEAN-200.5>",
-					["num"] = 3,
+					["CLSID"] = "{05544F1A-C39C-466b-BC37-5BD1D52E57BB}",
+					["num"] = 2,
 				},
 				[5] = {
-					["CLSID"] = "<CLEAN-200.5>",
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
 					["num"] = 4,
 				},
 				[6] = {
-					["CLSID"] = "<CLEAN>",
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["num"] = 3,
+				},
+			},
+			["tasks"] = {
+				[1] = 35,
+			},
+		},
+		[9] = {
+			["displayName"] = "AT Rockets (120)",
+			["name"] = "AT Rockets (120)",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "PKT_7_62",
+					["num"] = 8,
+				},
+				[2] = {
+					["CLSID"] = "KORD_12_7",
+					["num"] = 7,
+				},
+				[3] = {
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["num"] = 6,
+				},
+				[4] = {
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
 					["num"] = 5,
+				},
+				[5] = {
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["num"] = 4,
+				},
+				[6] = {
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["num"] = 3,
+				},
+				[7] = {
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["num"] = 2,
+				},
+				[8] = {
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 35,
+			},
+		},
+		[10] = {
+			["displayName"] = "HE Rockets (80) | 12.7/7.62 Gun Pod",
+			["name"] = "HE Rockets (80) | 12.7/7.62 Gun Pod",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "PKT_7_62",
+					["num"] = 8,
+				},
+				[2] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 6,
+				},
+				[3] = {
+					["CLSID"] = "KORD_12_7",
+					["num"] = 7,
+				},
+				[4] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 1,
+				},
+				[5] = {
+					["CLSID"] = "GUV_YakB_GSHP",
+					["num"] = 5,
+				},
+				[6] = {
+					["CLSID"] = "GUV_YakB_GSHP",
+					["num"] = 2,
+				},
+				[7] = {
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["num"] = 4,
+				},
+				[8] = {
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["num"] = 3,
+				},
+			},
+			["tasks"] = {
+				[1] = 35,
+				[2] = 31,
+			},
+		},
+		[11] = {
+			["displayName"] = "HE/AT ROCKETS (80)",
+			["name"] = "HE/AT ROCKETS (80)",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "PKT_7_62",
+					["num"] = 8,
+				},
+				[2] = {
+					["CLSID"] = "KORD_12_7",
+					["num"] = 7,
+				},
+				[3] = {
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["num"] = 4,
+				},
+				[4] = {
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["num"] = 3,
+				},
+				[5] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 5,
+				},
+				[6] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 2,
+				},
+			},
+			["tasks"] = {
+				[1] = 35,
+				[2] = 32,
+			},
+		},
+		[12] = {
+			["name"] = "HE Rockets (120)",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "PKT_7_62",
+					["num"] = 8,
+				},
+				[2] = {
+					["CLSID"] = "KORD_12_7",
+					["num"] = 7,
+				},
+				[3] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 6,
+				},
+				[4] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 5,
+				},
+				[5] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 4,
+				},
+				[6] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 3,
+				},
+				[7] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 2,
+				},
+				[8] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 35,
+			},
+		},
+		[13] = {
+			["displayName"] = "Grenades | 12.7/7.62 Gun Pod | HE Rockets",
+			["name"] = "Grenades | 12.7/7.62 Gun Pod | HE Rockets",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "GUV_VOG",
+					["num"] = 6,
+				},
+				[2] = {
+					["CLSID"] = "GUV_VOG",
+					["num"] = 1,
+				},
+				[3] = {
+					["CLSID"] = "GUV_YakB_GSHP",
+					["num"] = 2,
+				},
+				[4] = {
+					["CLSID"] = "GUV_YakB_GSHP",
+					["num"] = 5,
+				},
+				[5] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 4,
+				},
+				[6] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 3,
 				},
 				[7] = {
 					["CLSID"] = "PKT_7_62",
@@ -289,104 +481,6 @@ local unitPayloads = {
 				[8] = {
 					["CLSID"] = "KORD_12_7",
 					["num"] = 7,
-				},
-			},
-			["tasks"] = {
-				[1] = 35,
-			},
-		},
-		[9] = {
-			["name"] = "AA Clean",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "<CLEAN>",
-					["num"] = 6,
-				},
-				[2] = {
-					["CLSID"] = "<CLEAN>",
-					["num"] = 1,
-				},
-				[3] = {
-					["CLSID"] = "<CLEAN>",
-					["num"] = 2,
-				},
-				[4] = {
-					["CLSID"] = "<CLEAN-200.5>",
-					["num"] = 3,
-				},
-				[5] = {
-					["CLSID"] = "<CLEAN-200.5>",
-					["num"] = 4,
-				},
-				[6] = {
-					["CLSID"] = "<CLEAN>",
-					["num"] = 5,
-				},
-			},
-			["tasks"] = {
-				[1] = 35,
-			},
-		},
-		[10] = {
-			["displayName"] = "AA Guns",
-			["name"] = "AA Guns",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "PKT_7_62",
-					["num"] = 8,
-				},
-				[2] = {
-					["CLSID"] = "KORD_12_7",
-					["num"] = 7,
-				},
-				[3] = {
-					["CLSID"] = "{05544F1A-C39C-466b-BC37-5BD1D52E57BB}",
-					["num"] = 5,
-				},
-				[4] = {
-					["CLSID"] = "{05544F1A-C39C-466b-BC37-5BD1D52E57BB}",
-					["num"] = 2,
-				},
-				[5] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
-					["num"] = 4,
-				},
-				[6] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
-					["num"] = 3,
-				},
-			},
-			["tasks"] = {
-				[1] = 35,
-			},
-		},
-		[11] = {
-			["displayName"] = "A",
-			["name"] = "A",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "PKT_7_62",
-					["num"] = 8,
-				},
-				[2] = {
-					["CLSID"] = "KORD_12_7",
-					["num"] = 7,
-				},
-				[3] = {
-					["CLSID"] = "B_8V20A_OFP2",
-					["num"] = 6,
-				},
-				[4] = {
-					["CLSID"] = "B_8V20A_OFP2",
-					["num"] = 1,
-				},
-				[5] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
-					["num"] = 4,
-				},
-				[6] = {
-					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
-					["num"] = 3,
 				},
 			},
 			["tasks"] = {
