37003e28863000bfe6289d530d6b8847be6bb4ba
Brendan Law <brendan.law.contractor@pepsico.com>
2023-09-19 21:10:01 -0700

    Added second profile for Gazelle since FAC switches from F1 to F3 sometimes

--- src/parse_key_stroke.ps1
+++ src/parse_key_stroke.ps1
@@ -1,4 +1,4 @@
-[XML]$vap = Get-Content 'VoiceAttack\Gazelle (RH)\Gazelle (Rotorheads) 0001-Profile.vap'
+[XML]$vap = Get-Content 'VoiceAttack\Gazelle (RH)\Gazelle (Rotorheads) 0002-Profile.vap'
 
 # prints 220
 #$vap.Profile.Commands.Command[0].ActionSequence.CommandAction[0].KeyCodes.unsignedShort
@@ -16,17 +16,17 @@ foreach ($command in $vap.Profile.Commands.Command)
   if($codes[0] -eq 220) { # \
     if($codes[1] -eq 121){ # F10
       if($codes[2] -eq 112){ #F1
-        # change to 113
-        Write-Host "Updating F1 to F2"
-        $codes[2] = 113
-      }elseif ($codes[2] -eq 113) { #F2
         # change to 114
-        Write-Host "Updating F2 to F3"
+        Write-Host "Updating F1 to F3"
         $codes[2] = 114
-      }elseif ($codes[2] -eq 114) { #F2
+      }elseif ($codes[2] -eq 113) { #F2
+        # change to 112
+        Write-Host "Updating F2 to F1"
+        $codes[2] = 112
+      }elseif ($codes[2] -eq 114) { #F3
         # change to 115
-        Write-Host "Updating F3 to F4"
-        $codes[2] = 115
+        Write-Host "Updating F3 to F2"
+        $codes[2] = 113
       }
     }
   }
